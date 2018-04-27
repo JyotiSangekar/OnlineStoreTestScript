d5fa8705c0a56c86b341ce9dab9b0f5ec2c8068f
karthik <kbellaryvijaya@gmail.com>
2018-04-27 02:32:20 -0600

    test scripts

---
+++ Add_Product.py
@@ -0,0 +1,36 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from datetime import datetime
+
+
+#
+class Test_addProduct(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    def test_user_login(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000")
+        # driver.get("http://127.0.0.1:8000/login") -
+        driver.find_element_by_xpath('//*[@id="main"]/div[1]').click()
+        time.sleep(3)
+        driver.find_element_by_xpath('//*[@id="content"]/div/form/input[3]').click()
+        time.sleep(3)
+        driver.find_element_by_xpath('//*[@id="content"]/table/tbody/tr[1]/td[4]/a').click()
+        time.sleep(5)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
